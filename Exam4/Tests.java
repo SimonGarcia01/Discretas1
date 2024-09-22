94166d77fe6376b03b3c729c75abccde54356c8a
Simón García <157841549+SimonGarcia01@users.noreply.github.com>
2024-09-22 16:52:04 -0500

    Added all additional tests to fit brief.

--- Exam4/Tests.java
+++ Exam4/Tests.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.List;
+import java.util.ConcurrentModificationException;
 
 public class Tests {
 
@@ -53,6 +54,19 @@ public class Tests {
         set2.add(6);
     }
 
+    public void repeatSetup(){
+        set1 = new Set<>();
+        set2 = new Set<>();
+
+        set1.add(1);
+        set1.add(1);
+        set1.add(1);
+
+        set2.add(1);
+        set2.add(1);
+        set2.add(1);
+    }
+
     @Test
     public void createSetTest(){
         //Init
@@ -65,6 +79,7 @@ public class Tests {
         assertNotNull(set1);
     }
 
+    //ADD TESTS
     @Test
     public void addTest(){
         //Init
@@ -79,6 +94,40 @@ public class Tests {
     }
 
     @Test
+    public void addTestRepeat(){
+        //Init
+        oneSetSetup();
+
+        //Act
+        set1.add(1);
+        set1.add(1);
+        set1.add(2);
+        set1.add(2);
+        set1.add(3);
+        set1.add(3);
+
+        //Assert
+        assertEquals("{1, 2, 3}", set1.toString());
+    }
+
+    @Test
+    public void addDeleteAddTest(){
+        //Init
+        oneSetSetup();
+
+        //Act
+        set1.add(99);
+        set1.delete(99);
+        set1.delete(2);
+        set1.add(99);
+        set1.add(02);
+
+        //Assert
+        assertEquals("{1, 3, 99, 2}", set1.toString());
+    }
+
+    //DELETE TESTS
+    @Test
     public void deleteTest(){
         //Init
         oneSetSetup();
@@ -91,6 +140,39 @@ public class Tests {
     }
 
     @Test
+    public void deleteNull(){
+        //Init
+        defaultSetup();
+
+        //Act
+        String message = "";
+        try{
+            set1.delete(1);
+        } catch (NullPointerException e) {
+            message = e.getMessage();
+        }
+
+        //Assert
+        assertEquals("Cannot invoke \"ISet.delete(Object)\" because \"this.set1\" is null", message);
+    }
+
+    @Test
+    public void deleteEmpty(){
+        //Init
+        defaultSetup();
+
+        //Act
+        set1 = new Set<>();
+        set1.add(1);
+        set1.delete(1);
+        set1.delete(1);
+
+        //Assert
+        assertEquals("{}", set1.toString());
+    }
+
+    //FIND ELEMENT TESTS
+    @Test
     public void findElementTest(){
         //Init
         oneSetSetup();
@@ -102,6 +184,31 @@ public class Tests {
         assertEquals(1, foundElement);
     }
 
+    @Test
+    public void findElementinOtherSet(){
+        //Init
+        twoSetSetup();
+
+        //Act
+        int foundElement = set1.findElement(set2.getAllElements().get(1));
+
+        //Assert
+        assertEquals(4, foundElement);
+    }
+
+    @Test
+    public void findRepeatedElement(){
+        //Init
+        repeatSetup();
+
+        //Act
+        int foundElement = set1.findElement(1);
+
+        //Assert
+        assertEquals(0,foundElement);
+    }
+
+    //GETALLELEMENTS TESTS
     @Test
     public void getAllElementsTest(){
         //Init
@@ -116,6 +223,32 @@ public class Tests {
     }
 
     @Test
+    public void getAllElementsEmptySet(){
+        //Init
+        defaultSetup();
+
+        //Act
+        set1 = new Set<>();
+        
+        //Assert
+        assertTrue(set1.getAllElements().isEmpty());
+    }
+
+    @Test
+    public void getAllElementsSubList(){
+        //Init
+        oneSetSetup();
+
+        //Act
+        set1.add(4);
+        set1.add(5);
+
+        //Assert
+        assertEquals("[4, 5]", set1.getAllElements().subList(3,5).toString());
+    }
+
+    //UNION TESTS
+    @Test
     public void unionTest(){
          //Init
         twoSetSetup();
@@ -127,6 +260,32 @@ public class Tests {
         assertEquals("{1, 2, 3, 4, 5, 6, 7, 8, 9}", set1.toString());
     }
 
+    @Test
+    public void unionItselfTest(){
+        //Init
+        twoSetSetup();
+
+        //Act
+        set1.union(set1);
+
+        //Assert
+        assertEquals("{1, 2, 3, 4, 5}", set1.toString());
+    }
+
+    @Test
+    public void nestedUnion(){
+        //Init
+        twoSetSetup();
+
+        //Act
+        set2.union(set1);
+        set1.union(set2);
+
+        //Assert
+        assertEquals("{1, 2, 3, 4, 5, 6, 7, 8, 9}", set1.toString());
+    }
+
+    //INTERSECTION TESTS
     @Test
     public void intersectionTest(){
         //Init
@@ -140,7 +299,37 @@ public class Tests {
     }
 
     @Test
-    public void differenceTest(){
+    public void emptyIntersection(){
+        //Init
+        twoSetSetup();
+
+        //Act
+        set1.delete(1);
+        set1.delete(2);
+        set1.delete(3);
+        set1.delete(4);
+        set1.delete(5);
+        set2.intersection(set1);
+
+        //Assert
+        assertEquals("{}", set1.toString());
+    }
+
+    @Test
+    public void subSetIntersection(){
+        //Init
+        subSetSetup();
+
+        //Act
+        set1.intersection(set2);
+
+        //Assert
+        assertEquals("{1, 2, 3}", set1.toString());
+    }
+
+    //DIFFERENCE TESTS
+    @Test
+    public void set1Set2differenceTest(){
         //Init
         twoSetSetup();
 
@@ -152,6 +341,36 @@ public class Tests {
     }
 
     @Test
+    public void set2Set1Difference(){
+                //Init
+                twoSetSetup();
+
+                //Act
+                set1.difference(set2, 2);
+        
+                //Assert
+                assertEquals("{6, 7, 8, 9}", set1.toString());
+    }
+
+    @Test
+    public void selfDifferenceTest(){
+        //init
+        oneSetSetup();
+
+        //Act
+        ConcurrentModificationException e = null;
+        try {
+            set1.difference(set1, 1);
+        } catch (ConcurrentModificationException ex) {
+            e = ex;
+        }
+
+        // Assert
+        assertNotNull("Expected ConcurrentModificationException", e);
+    }
+
+    //ISSUBSETTESTS
+    @Test
     public void isSubsetTest(){
         //Init
         subSetSetup();
@@ -161,9 +380,33 @@ public class Tests {
 
         //Assert
         assertTrue(isSet1SubetOfSet2);
-        
     }
-    
+
+    @Test
+    public void nonSubSetIsSubSetTest(){
+        //Init
+        twoSetSetup();
+
+        //Act
+        boolean isSubsetTest = set1.isSubset(set2);
+
+        //Assert
+        assertFalse(isSubsetTest);
+    }
+
+    @Test
+    public void selfSubSetTest(){
+        //Init
+        oneSetSetup();
+
+        //Act
+        boolean isSubsetTest = set1.isSubset(set1);
+
+        //Assert
+        assertTrue(isSubsetTest);
+    }
+
+    //COMPLIMENT TESTS
     @Test
     public void complementTest(){
         //Init
@@ -173,5 +416,34 @@ public class Tests {
 
         //Assert
         assertEquals("{4, 5, 6}", set1.toString());
+    }
+
+    @Test
+    public void selfComplement(){
+        // Init
+        oneSetSetup();
+
+        // Act
+        ConcurrentModificationException e = null;
+        try {
+            set1.complement(set1);
+        } catch (ConcurrentModificationException ex) {
+            e = ex;
+        }
+
+        // Assert
+        assertNotNull("Expected ConcurrentModificationException", e);
+    }
+
+    @Test
+    public void UniverseToSubSetComplementTest(){
+        //Init
+        subSetSetup();
+
+        //Act
+        set2.complement(set1);
+
+        //Assert
+        assertEquals("{1, 2, 3, 4, 5, 6}", set2.toString());
     }
 }
