36b04e03d48b573b7a0487870374013025eb3d7f
Simón García <157841549+SimonGarcia01@users.noreply.github.com>
2024-09-21 20:18:03 -0500

    Finished the Set implementation with generics and implemented tests for all methods.

--- Exam4/Set.java
+++ Exam4/Set.java
@@ -11,30 +11,55 @@ public class Set<T extends Comparable<T>> implements ISet<T>{
     }
 
     public void add(T element){
-        if(findElement(element) != -1){
+        if(findElement(element) == -1){
             allElements.add(element);
         }
     }
 
     public void delete(T element){
         int deletingE= findElement(element);
-
         if(deletingE != -1){
             allElements.remove(deletingE);
         }
-
-        allElements.remove(element);
     }
 
     public void union(ISet<T> set){
-
+        List<T> elements = set.getAllElements();
+        for(T element : elements){
+            this.add(element);
+        }
     }
+
     public void intersection(ISet<T> set){
+        List<T> elements = set.getAllElements();
+        List<T> newElements = new ArrayList<>();
 
-    }
-    public void difference(ISet<T> set){
+        for(T element : elements){
+            if(findElement(element) != -1){
+                newElements.add(element);
+            }
+        }
 
+        allElements = newElements;
     }
+
+    public void difference(ISet<T> set, int comparedSet){
+        List<T> elements = set.getAllElements();
+        
+        if(comparedSet == 1){
+            for(T element : elements){
+                delete(element);
+            }
+
+        } else if(comparedSet == 2){
+            for(T element : allElements){
+                elements.remove(element);
+            }
+
+            allElements = elements;
+        }
+    }
+    
     public List<T> getAllElements(){
         return this.allElements;
     }
@@ -45,6 +70,7 @@ public class Set<T extends Comparable<T>> implements ISet<T>{
         for(int n = 0; n < allElements.size() && !isFound; n++){
             if(allElements.get(n).compareTo(element) == 0){
                 found = n;
+                isFound = true;
             }
         }
 
@@ -52,10 +78,46 @@ public class Set<T extends Comparable<T>> implements ISet<T>{
     }
 
     public void complement(ISet<T> set){
-
+        if(isSubset(set)){
+            difference(set, 2);
+        }
     }
 
     public boolean isSubset(ISet<T> set){
+        boolean checkSubset =  true;
+        List<T> elements = set.getAllElements();
 
+        for(int n = 0; n < allElements.size() && checkSubset; n++){
+            boolean isFound = false;
+            for(int i = 0; i < elements.size() && !isFound; i++){
+                if(allElements.get(n).compareTo(elements.get(i)) == 0){
+                    isFound = true;
+                } 
+            }
+            if(!isFound){
+                checkSubset = false;
+            }
+        }
+
+        return checkSubset;
+    }
+
+    @Override
+    public String toString(){
+        String message = "{";
+
+        for(int n = 0; n < allElements.size(); n++){
+            if(n == 0){
+                message += allElements.get(n) ;
+            } else {
+                message += " " + allElements.get(n) ;
+            }
+
+            if(n < allElements.size() - 1){
+                message += ",";
+            }
+        }
+
+        return message + "}";
     }
 }
--- Exam4/Tests.java
+++ Exam4/Tests.java
@@ -1,10 +1,177 @@
 import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.List;
 
 public class Tests {
 
+    private ISet<Integer> set1;
+    private ISet<Integer> set2;
+
+    public void defaultSetup(){
+        set1 = null;
+    }
+
+    public void oneSetSetup(){
+        set1 = new Set<>();
+
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+    }
+
+    public void twoSetSetup(){
+        set1 = new Set<>();
+        set2 = new Set<>();
+
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+        set2.add(7);
+        set2.add(8);
+        set2.add(9);
+    }
+
+    public void subSetSetup(){
+        set1 = new Set<>();
+        set2 = new Set<>();
+
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        set2.add(1);
+        set2.add(2);
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+    }
+
     @Test
-    public void test(){
+    public void createSetTest(){
+        //Init
+        defaultSetup();
+
+        //Act
+        set1 = new Set<>();
+
+        //Assert
+        assertNotNull(set1);
+    }
+
+    @Test
+    public void addTest(){
+        //Init
+        oneSetSetup();
+
+        //Act
+        set1.add(2);
+        set1.add(4);
+
+        //Assert
+        assertEquals("{1, 2, 3, 4}", set1.toString());
+    }
+
+    @Test
+    public void deleteTest(){
+        //Init
+        oneSetSetup();
+
+        //Act
+        set1.delete(2);
+
+        //Assert
+        assertEquals("{1, 3}", set1.toString());
+    }
+
+    @Test
+    public void findElementTest(){
+        //Init
+        oneSetSetup();
+
+        //Act
+        int foundElement = set1.findElement(2);
+
+        //Assert
+        assertEquals(1, foundElement);
+    }
+
+    @Test
+    public void getAllElementsTest(){
+        //Init
+        oneSetSetup();
+
+        //Act
+        List<Integer> elements = set1.getAllElements();
+
+        //Assert
+        assertNotNull(elements);
+        assertTrue(elements.contains(2));
+    }
+
+    @Test
+    public void unionTest(){
+         //Init
+        twoSetSetup();
+        
+        //Act
+        set1.union(set2);
+
+        //Assert
+        assertEquals("{1, 2, 3, 4, 5, 6, 7, 8, 9}", set1.toString());
+    }
+
+    @Test
+    public void intersectionTest(){
+        //Init
+        twoSetSetup();
+
+        //Act
+        set1.intersection(set2);
+        
+        //Assert
+        assertEquals("{4, 5}", set1.toString());
+    }
+
+    @Test
+    public void differenceTest(){
+        //Init
+        twoSetSetup();
+
+        //Act
+        set1.difference(set2, 1);
+
+        //Assert
+        assertEquals("{1, 2, 3}", set1.toString());
+    }
+
+    @Test
+    public void isSubsetTest(){
+        //Init
+        subSetSetup();
+
+        //Act
+        boolean isSet1SubetOfSet2 = set1.isSubset(set2);
+
+        //Assert
+        assertTrue(isSet1SubetOfSet2);
         
     }
     
+    @Test
+    public void complementTest(){
+        //Init
+        subSetSetup();
+        //Act
+        set1.complement(set2);
+
+        //Assert
+        assertEquals("{4, 5, 6}", set1.toString());
+    }
 }
